b070125a9a0b3d73a9f13bba48c41bdb9d736657
UjjwalTyagi-2749 <gamingcyber805@gmail.com>
2026-06-30 11:50:18 +0530

    Time: 0 ms (100%), Space: 8.6 MB (67.34%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -3,21 +3,14 @@ public:
     bool isSubsequence(string s, string t) {
         int l=0;
         int r=0;
-        int c=0;
-        int count =t.size();
-        while(r<count){
+        while(r<t.size()){
             if(s[l]==t[r]){
                 l++;
                 r++;
-                c++;
             }else{
                 r++;
             }
         }
-        if(c==s.size()){
-            return true;
-        }else{
-            return false;
-        }
+        return l==s.size();
     }
 };
