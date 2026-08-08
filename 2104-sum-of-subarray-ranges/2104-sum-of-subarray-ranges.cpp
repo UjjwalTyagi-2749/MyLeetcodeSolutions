b9428e28ade8d58b7308763f3f3156d4c996b4d3
Ujjwal Tyagi <gamingcyber805@gmail.com>
2026-08-08 20:50:59 +0530

    Time: 2 ms (79.51%), Space: 15.6 MB (67.76%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,20 +1,91 @@
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& nums) {
-        int n = nums.size();
-        long long ans = 0;
+    long long max(vector<int>& nums){
+        int n=nums.size();
+        vector<int>left(n),right(n);
+        stack<int>s;
 
-        for (int i = 0; i < n; i++) {
-            int mini = nums[i];
-            int maxi = nums[i];
+        for(int i=0;i<n;i++){
 
-            for (int j = i; j < n; j++) {
-                mini = min(mini, nums[j]);
-                maxi = max(maxi, nums[j]);
-                ans += maxi - mini;
+            while(!s.empty() && nums[s.top()]<nums[i]){
+                s.pop();
             }
+            if(s.empty()){
+                left[i]=i+1;
+            }
+            else{
+                left[i]=i-s.top();
+            }
+            s.push(i);
         }
+        while(!s.empty()) s.pop();
 
-        return ans;
+        for(int j=n-1;j>=0;j--){
+
+            while(!s.empty() && nums[s.top()]<=nums[j]){
+                s.pop();
+            }
+            if(s.empty()){
+                right[j]=n-j;
+            }
+            else{
+                right[j]=s.top()-j;
+            }
+            s.push(j);
+        }
+        long long sum=0;
+
+        for(int i=0;i<n;i++){
+            sum+=(long long )nums[i]*left[i]*right[i];
+        }
+        return sum;
+    }
+
+    long long min(vector<int>& nums){
+        int n=nums.size();
+        vector<int>left(n),right(n);
+        stack<int>s;
+        
+
+        for(int i=0;i<n;i++){
+
+            while(!s.empty() && nums[s.top()]>nums[i]){
+                s.pop();
+            }
+            if(s.empty()){
+                left[i]=i+1;
+            }
+            else{
+                left[i]=i-s.top();
+            }
+            s.push(i);
+        }
+        while(!s.empty()) s.pop();
+
+        for(int j=n-1;j>=0;j--){
+
+            while(!s.empty() && nums[s.top()]>=nums[j]){
+                s.pop();
+            }
+            if(s.empty()){
+                right[j]=n-j;
+            }
+            else{
+                right[j]=s.top()-j;
+            }
+            s.push(j);
+        }
+        long long sum=0;
+
+        for(int i=0;i<n;i++){
+            sum+=(long long)nums[i]*left[i]*right[i];
+        }
+        return sum;
+    }
+
+    long long subArrayRanges(vector<int>& nums) {
+        long long mini=min(nums);
+        long long maxi=max(nums);
+        return maxi-mini;
     }
 };
